e10347507128bb370bc86bed686e59275e17a163
Ashok <ashokudhaya29@gmail.com>
2026-04-13 16:31:41 +0530

    fix model name

--- speech.py
+++ speech.py
@@ -4,15 +4,17 @@ import assemblyai as aai
 aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
 
 def speech_to_text(file_path):
-    print("🎯 Sending to AssemblyAI...")
+    try:
+        transcriber = aai.Transcriber()
 
-    config = aai.TranscriptionConfig(
-        speech_model="universal"
-    )
+        config = aai.TranscriptionConfig(
+            speech_models=["universal"]
+        )
 
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(file_path, config=config)
+        transcript = transcriber.transcribe(file_path, config=config)
 
-    print("✅ Transcribed:", transcript.text)
+        return transcript.text
 
-    return transcript.text
+    except Exception as e:
+        print("❌ ERROR in speech:", e)
+        return ""
