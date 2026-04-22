4a3f06bfdb444e33dc41a11234a72dd0f5c8f6d4
Ashok <ashokudhaya29@gmail.com>
2026-04-22 14:32:23 +0530

    fix speech function

--- speech.py
+++ speech.py
@@ -2,3 +2,14 @@ import assemblyai as aai
 import os
 
 aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")  
+
+def speech_to_text(file_path):
+    transcriber = aai.Transcriber()
+
+    config = aai.TranscriptionConfig(
+        speech_model=aai.SpeechModel.best
+    )
+
+    transcript = transcriber.transcribe(file_path, config=config)
+
+    return transcript.text
