070223c828b6ad469349116e4ab970b2241b22ba
ashokudhaya29-pixel <ashokudhaya29@gmail.com>
2026-05-03 16:50:58 +0530

    add dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,10 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
